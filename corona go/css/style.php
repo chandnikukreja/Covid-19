413f3f777282f034ab2d2c3f82183b5aa31b47da
chandni kukreja <76688876+chandnikukreja@users.noreply.github.com>
2021-01-01 17:42:25 +0530

    Add files via upload

---
+++ corona go/css/style.php
@@ -0,0 +1,21 @@
+<style type="text/css">
+*{ margin:0; padding:0; box-sizing:border-box;font-family: 'Mulish', sans-serif;}
+.nav_style{
+	background-color: #a29bfe!important;
+}
+.nav_style a {
+	color:white;
+}
+.corona_update{
+	margin:0 0 30px,0;
+}
+.corona_update h3{
+	color:#ff7675;
+}
+.sub_section{
+	background-color:#fbfafd;
+}
+.footer_style {
+	background-color:#a29bfe!important;
+}
+</style>
